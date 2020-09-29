aba8f4a3371cf21e4a5c3f22a0dbc7d9c2cd7b1f
Nick <niiickolay@gmail.com>
2020-09-29 13:51:10 +0300

    Add Inode, implement KDNode class

---
+++ src/node.ts
@@ -0,0 +1,24 @@
+import { IPoint2D } from './point';
+
+export interface INode {
+  point: IPoint2D;
+  left: Node;
+  right: Node;
+  parent: Node;
+  cd: number;
+}
+
+export type Node = INode | null;
+
+class KDNode implements INode {
+  left: Node = null
+  right: Node = null
+  parent: Node = null
+  cd: number = 0
+
+  constructor(public point: IPoint2D, cd: number) {
+    this.cd = cd;
+  }
+}
+
+export default KDNode;
