614da37f65bd50d02d775a8487eca6f417b4996b
Nick <niiickolay@gmail.com>
2020-10-02 13:04:42 +0300

    Add Rect2d type, change Rect class

--- src/rect.ts
+++ src/rect.ts
@@ -1,42 +1,27 @@
 import { Point2D } from './types/point2d';
+import { Rect2d } from './types/rect2d';
 
-export interface Rectangle {
-  xMin: number;
-  yMin: number;
-  xMax: number;
-  yMax: number;
-}
-
-class Rect implements Rectangle {
-  constructor(
-    readonly xMin: number,
-    readonly yMin: number,
-    readonly xMax: number,
-    readonly yMax: number
-  ) {}
-
-  contains(point: Point2D): boolean {
+class Rect {
+  static contains(rect: Rect2d, point: Point2D): boolean {
     return (
-      point[0] <= this.xMax &&
-      point[1] <= this.yMax &&
-      point[0] >= this.xMin &&
-      point[1] >= this.yMin
+      point[0] <= rect[1][0] &&
+      point[1] <= rect[1][1] &&
+      point[0] >= rect[0][0] &&
+      point[1] >= rect[0][1]
     );
   }
 
-  distanceTo(point: Point2D): number {
-    const dx = Math.max(this.xMin - point[0], point[0] - this.xMax);
-    const dy = Math.max(this.yMin - point[1], point[1] - this.yMax);
-    return Math.sqrt(dx ** 2 + dy ** 2);
+  static intersects(rectA: Rect2d, rectB: Rect2d): boolean {
+    return (
+      rectA[0][0] <= rectB[1][0] &&
+      rectB[0][0] <= rectA[1][0] &&
+      rectA[0][1] <= rectB[1][1] &&
+      rectB[0][1] <= rectA[1][1]
+    );
   }
 
-  intersects(rect: Rectangle): boolean {
-    return (
-      rect.xMin <= this.xMax &&
-      this.xMin <= rect.xMax &&
-      rect.yMin <= this.yMax &&
-      this.yMin <= rect.yMax
-    );
+  static clone(rect: Rect2d): Rect2d {
+    return  [[...rect[0]], [...rect[1]]];
   }
 }
 
